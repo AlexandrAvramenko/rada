a6b138f8d6a0df77f9914727a38beb9c825ed78f
Aleksandr Avramenko <avramenko.av84@gmail.com>
2019-04-12 15:18:27 +0300

    transitions between pages zakonoproekty

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// second click on the dropdown button and follow the link.
+$(document).ready(function () {
+    $('.navbar .dropdown > a').click(function() {
+        if (!$(this).hasClass("parent-clicked")) {
+           $(this).addClass("parent-clicked");
+         } else {
+           location.href = this.href;
+         }
+       });
+});
