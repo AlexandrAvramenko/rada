6c97390ebcb7780b56c53951ccdabcdf8ea02fa9
Aleksandr Avramenko <avramenko.av84@gmail.com>
2019-04-16 18:23:04 +0300

    poryadku denni plenarnykh zasidan

---
+++ build/js/app.js
@@ -0,0 +1,16 @@
+// second click on the dropdown button and follow the link.
+$(document).ready(function () {
+	$('.navbar .dropdown > a').click(function () {
+		if (!$(this).hasClass("parent-clicked")) {
+			$(this).addClass("parent-clicked");
+		} else {
+			location.href = this.href;
+		}
+	});
+
+	// tooltip
+	$(function () {
+		$('[data-toggle="tooltip"]').tooltip();
+	});
+
+});
--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-// second click on the dropdown button and follow the link.
-$(document).ready(function () {
-    $('.navbar .dropdown > a').click(function() {
-        if (!$(this).hasClass("parent-clicked")) {
-           $(this).addClass("parent-clicked");
-         } else {
-           location.href = this.href;
-         }
-       });
-
-    $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
-});
+ //= ../node_modules/jquery/dist/jquery.js
+ //= ../node_modules/bootstrap/dist/js/bootstrap.bundle.js
+ 
