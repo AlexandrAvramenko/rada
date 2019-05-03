d22cb37d8ea2af67e9dd778cda314cf05ee7ea5b
Aleksandr Avramenko <avramenko.av84@gmail.com>
2019-05-03 10:08:22 +0300

    fixed bugs for menu

--- js/app.js
+++ js/app.js
@@ -1,26 +1,27 @@
 // second click on the dropdown button and follow the link.
 $(document).ready(function () {
-	$( '.dropdown-menu .dropdown-toggle' ).on('click', function() {
-    
+	$('.dropdown-menu .dropdown-toggle').on('click', function () {
+
 		var $el = $(this);
 		var $parent = $el.offsetParent(".dropdown-menu");
-		
+
 		if (!$el.next().hasClass("show")) {
 			$el.parents('.dropdown-menu').first().find(".show").removeClass("show");
 		}
 		$el.next(".dropdown-menu").toggleClass("show").parent("li").toggleClass("show");
-		
+
 		$el.parents("li.nav-item.dropdown.show").on("hidden.bs.dropdown", function () {
 			$(".dropdown-menu .show").removeClass("show");
-		});		
-		
+		});
+
 		if (!$parent.parent().hasClass("navbar-nav")) {
-			$el.next().css({"top":$el[0].offsetTop,"left":$parent.outerWidth()});
+			$el.next().css({ "top": $el[0].offsetTop, "left": $parent.outerWidth() });
 		}
-		
+
 		return false;
 	});
 
+	// menu double click
 	$('.navbar .dropdown > a').click(function () {
 		if (!$(this).hasClass("parent-clicked")) {
 			$(this).addClass("parent-clicked");
@@ -29,16 +30,16 @@ $(document).ready(function () {
 		}
 	});
 
-	//menu hidden submenu
-	$('.header').on('click',function(event){
-		if(!$(event.target).is('#nav-toggle')){
+	// menu hidden submenu
+	$('.header').on('click', function (event) {
+		if (!$(event.target).is('#nav-toggle')) {
 			$(".header").removeClass("visible");
 		}
-	 });
+	});
 
-	$( "#dropdown-visible" ).click(function() {
+	$("#dropdown-visible").click(function () {
 		$(".header").addClass("visible");
-	  });
+	});
 
 	// tooltip
 	$(function () {
